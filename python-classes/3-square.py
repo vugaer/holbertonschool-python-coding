bb9057adbff24bb08521f369af624eeca591344b
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-09 03:21:31 +0400

    gitpusher

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 """nice people making nice questions nice I AM RAGE
 BAITED"""
 
@@ -9,17 +10,23 @@ class Square:
     "people boss"
 
     def __init__(self, size=0):
-        self.__size = size
         if type(size) is not int:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
+        self.__size = size
 
     def area(self):
         return self.__size ** 2
 
-    def size(self, value=None):
-        if value == None:
-            return self.__size
-        else:
-            self.__size = value
+    def get_size(self):
+        return self.__size
+    
+    def set_size(self, value):
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+    
+    size = property(get_size, set_size)
