b32a0df1034102303af8eaac52343c38ccc434a1
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2025-12-09 02:07:50 +0400

    gitpusher

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -4,12 +4,11 @@
 Docstring for 0-square
 """
 
+
 class Square:
 
-    "do not hate me cuz I am beatiful white person"
+    """do not hate me cuz I am beatiful white person"""
 
     def __init__(self, size):
-
         """help me to get out of here"""
-
         self.__size = size
